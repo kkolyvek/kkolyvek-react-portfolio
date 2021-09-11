ffb4f487f61fac34324d77ae9e50cc288d1709b0
Koppi Kolyvek <kk674@cornell.edu>
2021-09-11 09:31:20 -0700

    deleted unnecesary files and added technical skills section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,38 +1,79 @@
-import React from 'react';
+import React from "react";
 
 // BOOTSTRAP IMPORT
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 // LOCAL IMPORTS
-import './About.css';
-import portrait from './assets/image-greenlake-removebg-cropped.png';
+import "./About.css";
+import portrait from "./assets/image-greenlake-removebg-cropped.png";
 
 function About() {
-    return (
-        <Container fluid className="custom-container">
-            <Container>
-                <Row style={{borderBottom: '2px solid #282828'}}>
-                    <Col sm={12} lg={6}>
-                        <div className="about-container">
-                            <h1 className="about-title">Koppi Kolyvek</h1>
-                            <div className="about-content">
-                                <p>Hi there, I'm Koppi. I'm an enthusiastic and quick-learning junior full stack software engineer from the Pacific Northwest starting my journey in the web development world.</p>
-                                <p>In design, I strive to produce elegant yet intuitive solutions that leave the user comfortable and satisfied.</p>
-                                <p>Though new to web development, I have a few years of experience in software as a computer vision engineer and research assistant - as well as a two year stint working in a small, agile structural engineering team.</p>
-                            </div>
-                        </div>
-                    </Col>
-                    <Col sm={12} lg={6}>
-                        <div className="image-wrapper">
-                            <img src={portrait} alt="Portrait of Koppi" className="portrait" />
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-        </Container>
-    );
+  return (
+    <Container fluid className="custom-container">
+      <Container>
+        <Row style={{ borderBottom: "2px solid #282828" }}>
+          <Col sm={12} lg={6}>
+            <div className="about-container">
+              <h1 className="about-title">Koppi Kolyvek</h1>
+              <div className="about-content">
+                <p>
+                  Hi there, I'm Koppi. I'm an enthusiastic and quick-learning
+                  junior full stack software engineer from the Pacific Northwest
+                  starting my journey in the web development world.
+                </p>
+                <p>
+                  In design, I strive to produce elegant yet intuitive solutions
+                  that leave the user comfortable and satisfied.
+                </p>
+                <p>
+                  Though new to web development, I have a few years of
+                  experience in software as a computer vision engineer and
+                  research assistant - as well as a two year stint working in a
+                  small, agile structural engineering team.
+                </p>
+              </div>
+              <h1 className="skills-title">Languages and Technical Skills</h1>
+              <div>
+                <span className="rounded-pill mx-1 my-1 skills-badge">
+                  JavaScript
+                </span>
+                <span className="rounded-pill mx-1 my-1 skills-badge">
+                  HTML
+                </span>
+                <span className="rounded-pill mx-1 my-1 skills-badge">CSS</span>
+                <span className="rounded-pill mx-1 my-1 skills-badge">
+                  Python
+                </span>
+                <span className="rounded-pill mx-1 my-1 skills-badge">
+                  ReactJS
+                </span>
+                <span className="rounded-pill mx-1 my-1 skills-badge">
+                  Handlebars.js
+                </span>
+                <span className="rounded-pill mx-1 my-1 skills-badge">
+                  MongoDB / Mongoose
+                </span>
+                <span className="rounded-pill mx-1 my-1 skills-badge">
+                  SQL / Sequelize
+                </span>
+              </div>
+            </div>
+          </Col>
+          <Col sm={12} lg={6}>
+            <div className="image-wrapper">
+              <img
+                src={portrait}
+                alt="Portrait of Koppi"
+                className="portrait"
+              />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
 }
 
 export default About;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 // import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import App from './App';
+import App from "./App";
 
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById("root"));
